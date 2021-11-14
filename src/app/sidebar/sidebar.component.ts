4de7b50570ee078c7278fcdcf13c50b02a7dd949
Eben Mallory <ebenryanmallory@gmail.com>
2021-11-13 22:43:25 -0600

    added sidebar links

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -28,6 +28,30 @@ export class SidebarComponent implements OnInit {
     this.todosCollection.add(todo);
   }
 
+  displayDashboard(emit: any) {
+    document.querySelectorAll('.active').forEach(active => active.classList.remove('active'));
+    emit.target.parentNode.classList.add('active');
+    document.querySelectorAll('.content-wrapper').forEach(wrapper => wrapper.classList.remove('hidden'));
+  }
+  displayTodos(emit: any) {
+    document.querySelectorAll('.active').forEach(active => active.classList.remove('active'));
+    emit.target.parentNode.classList.add('active');
+    document.querySelectorAll('.content-wrapper').forEach(wrapper => wrapper.classList.add('hidden'));
+    document.querySelectorAll('#todos').forEach(todos => todos.classList.remove('hidden'));
+  }
+  displayMovies(emit: any) {
+    document.querySelectorAll('.active').forEach(active => active.classList.remove('active'));
+    emit.target.parentNode.classList.add('active');
+    document.querySelectorAll('.content-wrapper').forEach(wrapper => wrapper.classList.add('hidden'));
+    document.querySelectorAll('#movies').forEach(movies => movies.classList.remove('hidden'));
+  }
+  displayNews(emit: any) {
+    document.querySelectorAll('.active').forEach(active => active.classList.remove('active'));
+    emit.target.parentNode.classList.add('active');
+    document.querySelectorAll('.content-wrapper').forEach(wrapper => wrapper.classList.add('hidden'));
+    document.querySelectorAll('#news').forEach(news => news.classList.remove('hidden'));
+  }
+
   ngOnInit(): void {
   }
 
