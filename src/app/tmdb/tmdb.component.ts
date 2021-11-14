aa25058bf7ec725796ff559dce7c05247641cba7
Eben Mallory <ebenryanmallory@gmail.com>
2021-11-13 20:56:52 -0600

    fixed movie poster glitch

--- src/app/tmdb/tmdb.component.ts
+++ src/app/tmdb/tmdb.component.ts
@@ -13,6 +13,9 @@ export class TmdbComponent implements OnInit {
   baseURL = 'https://www.themoviedb.org/t/p/w440_and_h660_face/';
   movies: Movie[] = [];
 
+  getFullPath = (posterpath: string) => {
+    return `background-image: url(${this.baseURL + posterpath});`;
+  }
   constructor(private movieService: MoviesService) { }
  
   ngOnInit() {
